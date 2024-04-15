771ee2aca6b5268b156d27f668e4042512f88771
dahekim <kimdre88@naver.com>
2024-04-16 01:41:42 +0900

    dahekim / 3월 11주차 / 2문제

--- dahekim/week 11/42_loginSuccess.js
+++ dahekim/week 11/42_loginSuccess.js
@@ -1,8 +1,7 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/120883
 
 function solution(id_pw, db) {
-    const [id, pw] = id_pw;
-    
+    const [id, pw] = id_pw;   
     const user = db.find(e => e[0] === id);
     
     return user ? (user[1] === pw ? "login" 
