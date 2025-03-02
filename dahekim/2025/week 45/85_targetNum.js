c1047e60d50766c2835625a3f70d6c9b0ae1a388
dahekim <kimdre88@naver.com>
2025-03-02 21:49:25 +0900

    dahekim / 3월 1주차 (45주차) / 1문제 (타겟 넘버)

---
+++ dahekim/2025/week 45/85_targetNum.js
@@ -0,0 +1,18 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/43165
+
+function solution(numbers, target) {
+    let answer = 0;
+
+    function dfs(index, sum) {
+        if (index === numbers.length) {
+            if (sum === target) answer++; 
+            return;
+        }
+
+        dfs(index + 1, sum + numbers[index]);
+        dfs(index + 1, sum - numbers[index]);
+    }
+
+    dfs(0, 0); 
+    return answer;
+}
