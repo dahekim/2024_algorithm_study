3d9ed55b8e48a27a4180d5e45fafba6078d85546
dahekim <kimdre88@naver.com>
2025-07-26 20:32:33 +0900

    dahekim / 7월 4주차 (59주차) / 1문제 (야근지수)

---
+++ dahekim/2025/week 59/99_nightwork.js
@@ -0,0 +1,22 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12927
+
+function solution(n, works) {
+  let answer = 0;
+
+  works.sort((a, b) => b - a);
+
+  while (n > 0) {
+    const max = Math.max(...works);
+    if (max === 0) break;
+
+    let idx = works.findIndex((v) => v === max);
+
+    while (max === works[idx] && n > 0) {
+      works[idx]--;
+      n--;
+      idx++;
+    }
+  }
+
+  return works.filter((v) => v !== 0).reduce((acc, cur) => acc + cur * cur, 0);
+}
